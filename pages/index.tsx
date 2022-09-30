353de9fa1468b9883f201111c334e9c1aaa513fd
ctd <qpflapffhs76@gmail.com>
2022-09-30 22:23:40 +0900

    🎉 Next.js boiler-plate with typescript, emotion, eslint, prettier, husky, lint-staged...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styled from '@emotion/styled';
+import styles from '../styles/Home.module.css';
+
+const Test = styled.div`
+  color: red;
+`;
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +17,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Test>Test</Test>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -66,7 +72,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
